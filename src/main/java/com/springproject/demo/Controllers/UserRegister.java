24d1fb2bfa7ed0d5e26d5f6dff3d98703f24ebc0
Balaji <balaji31@gmail.com>
2025-02-20 02:15:41 -0600

    removed unwanted imports

--- src/main/java/com/springproject/demo/Controllers/UserRegister.java
+++ src/main/java/com/springproject/demo/Controllers/UserRegister.java
@@ -3,7 +3,7 @@ package com.springproject.demo.Controllers;
 import com.springproject.demo.Model.User;
 import com.springproject.demo.Service.UserRegisteration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
